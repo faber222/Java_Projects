6e44c9746acf35dadb2a9e041f502d25421fe317
faber222 <faberbernardo78@gmail.com>
2022-09-15 23:29:04 -0300

    Exercício 03-b concluído

---
+++ lista-01/SegundaQuestão.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+
+public class SegundaQuestão {
+    public static void main(String[] args) {
+        char[][] board = new char[9][9];
+        int[][] mines = new int[9][9];
+        int line = 0, column = 0;
+        boolean drawnNumber = true;
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            while (drawnNumber) {
+                line = random.nextInt(8) + 1;
+                column = random.nextInt(8) + 1;
+                if (mines[line][column] != -1) {
+                    drawnNumber = false;
+                }
+            }
+            drawnNumber = true;
+            mines[line][column] = -1;
+        }
+        showBoard(board, mines, line, column);
+    }
+
+    public static void showBoard(char[][] board, int[][] mines, int line, int column) {
+
+        for (int i = 1; i < mines.length; i++) {
+            for (int j = 1; j < mines.length; j++) {
+                board[i][j] = '.';
+            }
+        }
+
+        for (int i = 1; i < 9; i++) {
+            for (int j = 1; j < 9; j++) {
+                if (mines[i][j] == -1) {
+                    board[i][j] = '*';
+                }
+            }
+        }
+
+        for (int line1 = 8; line1 > 0; line1--) {
+            for (int column1 = 1; column1 < 9; column1++) {
+                System.out.print(board[line1][column1]);
+            }
+            System.out.println();
+        }
+    }
+}
